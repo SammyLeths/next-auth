2c6753c272585338f49c5650212167dfbc7e2b62
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-05-09 19:18:55 +0100

    setup next authentication with bcrypt et al

---
+++ pages/auth.js
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import AuthForm from '../components/auth/auth-form';
+import { getSession } from 'next-auth/client';
+import { useState, useEffect } from 'react';
+
+function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace('/');
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return <AuthForm />;
+}
+
+export default AuthPage;
